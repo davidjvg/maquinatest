b1724e11ac1046b076ca7e4116bba96edc17244c
davidjvg <36414398+davidjvg@users.noreply.github.com>
2020-04-20 13:32:38 +0200

    .

--- main.tf
+++ main.tf
@@ -6,9 +6,15 @@ terraform {
 provider "azurerm" {
 
  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
-  version  = "1.39.0"
+  version  = "=1.42.0"
+  client_id = var.client_id
+  subscription_id = var.subscription_id
+  tenant_id = var.tenant_id
+  client_secret = var.client_secret
 
 }
+
+
 
 
 
